895016c8359ca1ce7cadcea3b4667253d3675d94
HebaRamadan90 <h05334686@gmail.com>
2026-05-08 14:15:36 +0300

    Delete masroofy/budget directory

--- masroofy/budget/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import dashboard, add_expense
-
-urlpatterns = [
-    path('', dashboard, name = 'dashboard'), # This will be the main page showing the dashboard
-    path('add-expense/', add_expense, name='add_expense'), # This will be the page to add a new expense  
-]
